ae8b9b8baa275a59a62e3fbd51c61618b712bf55
Ian Bruns <ian@rbruns.com>
2021-05-02 12:46:09 -0600

    moved app and stripped down boilerplate items

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function App(props: any) {
+  return (
+    <div className="App">
+      <header>
+        <h1>App</h1>
+      </header>
+    </div>
+  );
+}
